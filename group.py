d34a71fab87b03ba797f4574d955fe25739afeda
wangzongding <ucapzw2@ucl.ac.uk>
2023-10-19 10:36:41 +0100

    make dict

--- group.py
+++ group.py
@@ -2,4 +2,44 @@
 
 # Your code to go here...
 
-my_group =
+my_group = {'Jill':
+            {'age':26,
+             'job':'biologist',
+             'connection':
+             {'partner':'John',
+              'friend':['Zalika'],
+              'landlord': None,
+              'cousin':None,     
+             }
+            },
+            'John':
+            {'age':27,
+             'job':'writer',
+             'connection':
+             {'partner':'Jill',
+              'friend':[],
+              'landlord': None,
+              'cousin':'Nash',     
+             }
+            },
+            'Zalika':
+            {'age':28,
+             'job':'artist',
+             'connection':
+             {'partner': None,
+              'friend':['Jill'],
+              'landlord': 'Zalika',
+              'cousin':None,     
+             }
+            },
+            'Nash':
+            {'age':34,
+             'job':'Chef',
+             'connection':
+             {'partner':None,
+              'friend':[],
+              'landlord': None,
+              'cousin':None,     
+             }
+            },
+           }
